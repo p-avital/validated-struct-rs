547c8de96231dcc22d595a429b8660883dc9044e
Pierre Avital <pierre.avital@me.com>
2021-09-06 10:07:16 +0200

    initial commit

---
+++ validated_struct/examples/example.rs
@@ -0,0 +1,39 @@
+fn string_validator(new: &str) -> bool {
+    !new.is_empty()
+}
+fn hi_validator(new: &Hi) -> bool {
+    new.c().len() == *new.d()
+}
+validated_struct_macros::validator! {
+    #[recursive_attrs] // attributes bellow are added to each substructure, such as Hi
+    #[repr(C)]
+    #[derive(Clone, Debug, serde::Deserialize)]
+    Hello {
+        a: String where (string_validator),
+        b: Hi {
+            c: Vec<f64>,
+            d: usize
+        } where (hi_validator)
+    }
+}
+
+#[cfg(feature = "serde_json")]
+fn main() {
+    use validated_struct::ValidatedMap;
+    let from_str = serde_json::Deserializer::from_str;
+    let mut hello =
+        Hello::from_deserializer(&mut from_str(r#"{"a": "hi", "b": {"c": [0.1], "d":1}}"#))
+            .unwrap();
+    hello.insert("a", &mut from_str("\"\"")).unwrap_err();
+    hello.insert("a", &mut from_str("\"hello\"")).unwrap();
+    hello
+        .insert("b", &mut from_str(r#"{"c": [0.2, 0.1], "d":3}"#))
+        .unwrap_err();
+    hello
+        .insert("b", &mut from_str(r#"{"c": [0.2, 0.1], "d":2}"#))
+        .unwrap();
+}
+#[cfg(not(feature = "serde_json"))]
+fn main() {
+    panic!("You must build this example with --feature=serde_json for it to work")
+}
---
+++ validated_struct/src/lib.rs
@@ -0,0 +1,109 @@
+use std::{any::Any, collections::HashMap};
+#[derive(Default)]
+pub struct Validator {
+    root: Vec<Box<dyn Fn(&dyn Any) -> bool>>,
+    children: HashMap<String, Validator>,
+}
+impl Validator {
+    pub fn validate<K: IntoIterator<Item = S>, S: AsRef<str>, V: Any>(
+        &self,
+        path: K,
+        value: &V,
+    ) -> bool {
+        let mut path = path.into_iter();
+        if let Some(p) = path.next() {
+            if let Some(child) = self.children.get(p.as_ref()) {
+                child.validate(path, value)
+            } else {
+                true
+            }
+        } else {
+            self.root.iter().all(|f| f(value))
+        }
+    }
+    pub fn remove_predicates<K: IntoIterator<Item = S>, S: AsRef<str>>(
+        &mut self,
+        path: K,
+    ) -> Vec<Box<dyn Fn(&dyn Any) -> bool>> {
+        let mut path = path.into_iter();
+        if let Some(p) = path.next() {
+            if let Some(child) = self.children.get_mut(p.as_ref()) {
+                child.remove_predicates(path)
+            } else {
+                Vec::new()
+            }
+        } else {
+            let mut value = Vec::new();
+            std::mem::swap(&mut self.root, &mut value);
+            value
+        }
+    }
+    pub fn add_predicate<K: IntoIterator<Item = S>, S: Into<String>>(
+        &mut self,
+        path: K,
+        value: Box<dyn Fn(&dyn Any) -> bool>,
+    ) {
+        let mut path = path.into_iter();
+        if let Some(p) = path.next() {
+            let child = self.children.entry(p.into()).or_default();
+            child.add_predicate(path, value);
+        } else {
+            self.root.push(value)
+        }
+    }
+}
+#[derive(Debug)]
+pub enum InsertionError {
+    SyncInsertNotAvailable,
+    #[cfg(feature = "serde_json")]
+    JsonErr(serde_json::Error),
+    Str(&'static str),
+    String(String),
+}
+impl InsertionError {
+    pub fn sync_insert_not_available() -> Self {
+        InsertionError::SyncInsertNotAvailable
+    }
+}
+#[cfg(feature = "serde_json")]
+impl From<serde_json::Error> for InsertionError {
+    fn from(e: serde_json::Error) -> Self {
+        InsertionError::JsonErr(e)
+    }
+}
+#[derive(Debug, Clone, Copy)]
+pub enum GetError {
+    NoMatchingKey,
+    TypeMissMatch,
+}
+impl From<&'static str> for InsertionError {
+    fn from(s: &'static str) -> Self {
+        InsertionError::Str(s)
+    }
+}
+impl From<String> for InsertionError {
+    fn from(s: String) -> Self {
+        InsertionError::String(s)
+    }
+}
+pub trait ValidatedMap {
+    #[allow(unused_variables)]
+    fn insert_sync<'d, D: serde::Deserializer<'d>>(
+        &mut self,
+        key: &str,
+        value: D,
+    ) -> Result<(), InsertionError>
+    where
+        InsertionError: From<D::Error>,
+    {
+        Err(InsertionError::sync_insert_not_available())
+    }
+    fn insert<'d, D: serde::Deserializer<'d>>(
+        &mut self,
+        key: &str,
+        value: D,
+    ) -> Result<(), InsertionError>
+    where
+        InsertionError: From<D::Error>;
+    fn get<'a>(&'a self, key: &str) -> Result<&'a dyn Any, GetError>;
+}
