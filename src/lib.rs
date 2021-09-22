e3ca7dee561d221493149ef2890b83bdb2e115c5
Pierre Avital <pierre.avital@me.com>
2021-09-22 13:40:06 +0200

    add support for json5

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,13 @@
-use std::{any::Any, borrow::Cow, collections::HashMap};
+use std::any::Any;
 pub use validated_struct_macros::*;
-#[derive(Default)]
-pub struct Validator {
-    root: Vec<Box<dyn Fn(&dyn Any) -> bool>>,
-    children: HashMap<String, Validator>,
-}
-impl Validator {
-    pub fn validate<K: IntoIterator<Item = S>, S: AsRef<str>, V: Any>(
-        &self,
-        path: K,
-        value: &V,
-    ) -> bool {
-        let mut path = path.into_iter();
-        if let Some(p) = path.next() {
-            if let Some(child) = self.children.get(p.as_ref()) {
-                child.validate(path, value)
-            } else {
-                true
-            }
-        } else {
-            self.root.iter().all(|f| f(value))
-        }
-    }
-    pub fn remove_predicates<K: IntoIterator<Item = S>, S: AsRef<str>>(
-        &mut self,
-        path: K,
-    ) -> Vec<Box<dyn Fn(&dyn Any) -> bool>> {
-        let mut path = path.into_iter();
-        if let Some(p) = path.next() {
-            if let Some(child) = self.children.get_mut(p.as_ref()) {
-                child.remove_predicates(path)
-            } else {
-                Vec::new()
-            }
-        } else {
-            let mut value = Vec::new();
-            std::mem::swap(&mut self.root, &mut value);
-            value
-        }
-    }
-    pub fn add_predicate<K: IntoIterator<Item = S>, S: Into<String>>(
-        &mut self,
-        path: K,
-        value: Box<dyn Fn(&dyn Any) -> bool>,
-    ) {
-        let mut path = path.into_iter();
-        if let Some(p) = path.next() {
-            let child = self.children.entry(p.into()).or_default();
-            child.add_predicate(path, value);
-        } else {
-            self.root.push(value)
-        }
-    }
-}
+
 #[derive(Debug)]
 pub enum InsertionError {
     SyncInsertNotAvailable,
     #[cfg(feature = "serde_json")]
     JsonErr(serde_json::Error),
+    #[cfg(feature = "json5")]
+    Json5Err(json5::Error),
     Str(&'static str),
     String(String),
 }
@@ -76,6 +26,12 @@ impl InsertionError {
 impl From<serde_json::Error> for InsertionError {
     fn from(e: serde_json::Error) -> Self {
         InsertionError::JsonErr(e)
+    }
+}
+#[cfg(feature = "json5")]
+impl From<json5::Error> for InsertionError {
+    fn from(e: json5::Error) -> Self {
+        InsertionError::Json5Err(e)
     }
 }
 #[derive(Debug, Clone, Copy)]
